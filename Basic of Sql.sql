eb96952260b5801e04163cd0355621e038ba58e4
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-24 17:47:55 +0530

    Create Basic of Sql.sql

---
+++ Basic of Sql.sql
@@ -0,0 +1,18 @@
+--Distinct: For distinct values
+SELECT DISTINCT column1, column2 FROM table_name;
+
+--Where: Used for condition
+SELECT column1, column2 FROM table_name WHERE condition;
+
+--Order BY: The ORDER BY keyword is used to sort the result-set in ascending or descending order
+SELECT column1, column2 FROM table_name ORDER BY column1, column2, ... ASC|DESC;
+
+--Insert into:
+INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
+
+--Update:
+UPDATE table_name SET column1 = value1, column2 = value2 WHERE condition;
+
+--Delete
+DELETE FROM table_name WHERE condition;
+
